f23a1ca07d129483ecd11c18f1d055c02126e56a
Edi Mulyadi Rosdiman <51184976+edimulyadi@users.noreply.github.com>
2022-03-03 05:11:01 +0700

    fisrt commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,7 +10,6 @@ class Contact extends Component {
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
-      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -94,13 +93,11 @@ class Contact extends Component {
                         <span>
                         “Only you can change your life. Nobody else can do it for you.
                         </span>
-                        <b><a href="#">2 Days Ago</a></b>
                      </li>
                      <li>
                         <span>
                         "In the middle of a difficulty lies opportunity." - Albert Einstein
                         </span>
-                        <b><a href="#">3 Days Ago</a></b>
                      </li>
                   </ul>
 		         </div>
